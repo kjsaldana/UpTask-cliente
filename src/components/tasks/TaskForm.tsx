94a80203546aef064393e0c6ec68934bb082bd15
Kevin J <kjoseph1910@hotmail.com>
2026-03-16 17:27:05 +0100

    feat: Add responsable section to Task

--- src/components/tasks/TaskForm.tsx
+++ src/components/tasks/TaskForm.tsx
@@ -9,7 +9,8 @@ type TaskFormType = {
 
 export default function TaskForm({ register, errors }: TaskFormType) {
     return (
-        <div className="mb-5 space-y-3">
+        <div className="mb-5 space-y-4">
+            <label className='font-bold' htmlFor="name">Nombre de tarea:</label>
             <input
                 id="name"
                 className="w-full p-3 border border-gray-200"
@@ -21,7 +22,7 @@ export default function TaskForm({ register, errors }: TaskFormType) {
             />
 
             {errors.name && (<ErrorMessage>{errors.name.message}</ErrorMessage>)}
-
+            <label className='font-bold' htmlFor="description">Descripción:</label>
             <input
                 id="description"
                 className="w-full p-3  border border-gray-200"
@@ -33,6 +34,14 @@ export default function TaskForm({ register, errors }: TaskFormType) {
             />
 
             {errors.description && (<ErrorMessage>{errors.description.message}</ErrorMessage>)}
+            <label className='font-bold' htmlFor="responsable">Responsable:</label>
+            <input
+                id="responsable"
+                className="w-full p-3  border border-gray-200"
+                type="text"
+                placeholder="Responsable"
+                {...register("responsable")}
+            />
         </div>
     )
 }
